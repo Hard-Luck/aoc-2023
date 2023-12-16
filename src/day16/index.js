94468218507c6364f0e3bcb5e5a429090a2d84ad
Mark Tyree <nc-mark-t>
2023-12-16 14:52:22 +0000

    16.1 && 16.2

--- src/day16/index.js
+++ src/day16/index.js
@@ -1,63 +1,72 @@
 import run from "aocrunner";
 
 const parseInput = (rawInput) => rawInput;
-function handleForwardSlash([y, x]) {
-  return [[-x, -y]];
-}
-function handleBackSlash([y, x]) {
-  return [[x, y]];
-}
-function handleHorizontal([y, x]) {
-  if (y === 0) return [[y, x]];
-  return [
-    [-1, 0],
-    [1, 0],
-  ];
-}
-function handleVertical([y, x]) {
-  if (x === 0) return [[y, x]];
-  return [
-    [0, 1],
-    [0, -1],
-  ];
-}
-const moves = {
-  b: handleBackSlash,
-  "/": handleForwardSlash,
-  "-": handleHorizontal,
-  "|": handleVertical,
-};
-
-function draw(grid, location) {
-  console.log(location);
-  const [y, x] = location;
-  if (x < 0 || y < 0 || x >= grid[0].length || y >= grid.length) return;
+function draw(grid, [y, x]) {
   grid[y][x] = true;
 }
-function walk(grid, mapped, location, direction) {
-  const nextDirection = newDirection(grid, location, direction);
-  for (const d of nextDirection) {
-    const newLocation = [location[0] + d[0], location[1] + d[1]];
-    draw(mapped, newLocation);
-    const [y, x] = newLocation;
-    console.log({ newLocation, d });
-    if (x < 0 || y < 0 || x >= grid[0].length || y >= grid.length) return;
-    walk(grid, mapped, newLocation, d);
-  }
-  return mapped;
+function inBounds(grid, [y, x]) {
+  return x >= 0 && x < grid[0].length && y >= 0 && y < grid.length;
 }
-function newDirection(grid, [b, a], [dy, dx]) {
-  if (grid[b]?.[a] in moves) {
-    return moves[grid[b][a]]([dy, dx]);
+function walk(grid, mapped, { location, direction }) {
+  const nextDirections = newDirection(grid, location, direction);
+  const locations = [];
+  for (const d of nextDirections) {
+    const newLocation = [location[0] + d[0], location[1] + d[1]];
+    const [y, x] = newLocation;
+    if (!inBounds(grid, [y, x])) continue;
+    draw(mapped, newLocation);
+    locations.push({ location: newLocation, direction: d });
+  }
+  return locations;
+}
+function getFromLocation(grid, location) {
+  const [y, x] = location;
+  return grid[y][x];
+}
+function newDirection(grid, location, direction) {
+  const tile = getFromLocation(grid, location);
+  const [dy, dx] = direction;
+  if (tile === "/") {
+    return [[-dx, -dy]];
+  }
+  if (tile === "b") {
+    return [[dx, dy]];
+  }
+  if (tile === "|" && dy === 0) {
+    return [
+      [1, 0],
+      [-1, 0],
+    ];
+  }
+  if (tile === "-" && dx === 0) {
+    return [
+      [0, 1],
+      [0, -1],
+    ];
   }
   return [[dy, dx]];
 }
 
-function mapJourney(grid) {
+function mapJourney(grid, [sy, sx] = [0, 0], [dy, dx] = [0, 1]) {
+  const seenWithApproach = {};
   const visited = grid.map((x) =>
     Array.from({ length: grid[0].length }).fill(false),
   );
-  walk(grid, visited, [0, 0], [0, 1]);
+  const currentLocations = [{ location: [sy, sx], direction: [dy, dx] }];
+  visited[sy][sx] = true;
+  while (currentLocations.length) {
+    const { location, direction } = currentLocations.pop();
+    const next = walk(grid, visited, { location, direction });
+    next.forEach((x) => {
+      const label = `${x.location}${x.direction}`;
+      if (!seenWithApproach[label]) {
+        currentLocations.push(x);
+        seenWithApproach[label] = true;
+      } else {
+        seenWithApproach[label] = true;
+      }
+    });
+  }
   return visited;
 }
 
@@ -67,8 +76,11 @@ const part1 = (rawInput) => {
     .split("\n")
     .map((x) => x.split(""));
   const visited = mapJourney(input);
-  console.table(visited);
-  return visited.reduce((a, c) => {
+  //console.table(visited);
+  return sumEnergised(visited);
+};
+function sumEnergised(grid) {
+  return grid.reduce((a, c) => {
     return (
       a +
       c.reduce((b, d) => {
@@ -76,12 +88,29 @@ const part1 = (rawInput) => {
       }, 0)
     );
   }, 0);
-};
-
+}
 const part2 = (rawInput) => {
-  const input = parseInput(rawInput);
-
-  return;
+  const input = parseInput(rawInput)
+    .replaceAll("\\", "b")
+    .split("\n")
+    .map((x) => x.split(""));
+  let best = 0;
+  for (let i = 0; i < input[0].length; i++) {
+    const visitedDown = mapJourney(input, [0, i], [1, 0]);
+    const visitedUp = mapJourney(input, [input.length - 1, i], [-1, 0]);
+    best = Math.max(sumEnergised(visitedUp), sumEnergised(visitedDown), best);
+  }
+  for (let j = 0; j < input.length; j++) {
+    const visitedRight = mapJourney(input, [j, 0], [0, 1]);
+    const visitedLeft = mapJourney(input, [j, input.length[0] - 1], [0, -1]);
+    best = Math.max(
+      sumEnergised(visitedRight),
+      sumEnergised(visitedLeft),
+      best,
+    );
+  }
+  //console.table(visited);
+  return best;
 };
 
 run({
@@ -115,5 +144,5 @@ run({
     solution: part2,
   },
   trimTestInputs: true,
-  onlyTests: true,
+  onlyTests: false,
 });
