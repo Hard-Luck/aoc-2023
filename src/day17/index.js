9cc2d22a5e5e3b2a3ae5a77a4b996404ff5187a8
Mark Tyree <nc-mark-t>
2023-12-18 14:10:20 +0000

    17.1 && 17.2

--- src/day17/index.js
+++ src/day17/index.js
@@ -1,127 +1,109 @@
 import run from "aocrunner";
-const cache = {};
-/*
-cache = {
-  [2,1]: {
-    d : 10
-    dd : 8
-    uu : 7
-    ...etc
-  }
-}
+import { Heap } from "heap-js";
 
-*/
-const parseInput = (rawInput) => rawInput;
-function generateWeightGrid(input) {
-  const grid = input.map((_) =>
-    Array.from({ length: input[0].length }).fill(Infinity),
-  );
-  grid[0][0] = 0;
-  return grid;
+function parseInput(rawInput) {
+  return rawInput;
 }
-const directions = {
-  u: [-1, 0],
-  d: [1, 0],
-  l: [0, -1],
-  r: [0, 1],
-};
-function sumPoints([a, b], [c, d]) {
-  return [a + c, b + d];
-}
-function addSeenToCache(point, direction) {
-  cache[point] ??= {};
-  cache[point].seen = true;
-}
-function addValueToCache(point, direction, value) {
-  cache[point] ??= {};
-  cache[point].seen = true;
-}
-let best = Infinity;
-function distanceToEnd(grid, [y, x], [ey, ex], direction, currentWeight) {
-  if (cache[[y, x]]?.[direction]?.value !== undefined) {
-    return cache[[y, x]][direction];
-  }
-  if (cache[[y, x]]?.[direction]?.seen !== undefined) {
-    return Infinity;
-  }
-  addSeenToCache([y, x], direction);
-  if (!inBounds(grid, [y, x])) return;
-  currentWeight += grid[y][x];
-  console.log({ ex, ey, x, y });
-  if (ey === y && ex === x) {
-    console.log("here");
-    best = Math.min(currentWeight, best);
-    return currentWeight;
-  }
-  const neighbours = getNeighbours(grid, [y, x], direction);
-
-  const toCache = Math.min(
-    ...neighbours
-      .filter(
-        ({ next, direction }) => cache[[y, x]]?.[direction]?.seen !== true,
-      )
-      .map(({ next, direction }) => {
-        console.log({ next, neighbours });
-        return distanceToEnd(grid, next, [ey, ex], direction, currentWeight);
-      }),
-  );
-  addValueToCache([y, x], direction, toCache);
-  return toCache;
-}
-// go to each neighbour
-// keep track of direction
-// return infinity for 3x in same direction
-//
-function inBounds(grid, [y, x]) {
-  return y >= 0 && x >= 0 && x < grid[0].length && y < grid.length;
-}
-function getNeighbours(grid, point, [dy, dx]) {
-  const neighbours = [];
-  if (dy !== 0 || dx !== 1) {
-    const toAdd = [point[0], point[1] - 1];
-    if (inBounds(grid, toAdd)) {
-      neighbours.push({ next: toAdd, direction: "l" });
-    }
-  }
-  if (dy !== 0 || dx !== -1) {
-    const toAdd = [point[0], point[1] + 1];
-    if (inBounds(grid, toAdd)) {
-      neighbours.push({ next: toAdd, direction: "r" });
-    }
-  }
-  if (dy !== 1 || dx !== 0) {
-    const toAdd = [point[0] - 1, point[1]];
-    if (inBounds(grid, toAdd)) {
-      neighbours.push({ next: toAdd, direction: "u" });
-    }
-  }
-  if (dy !== -1 || dx !== 0) {
-    const toAdd = [point[0] + 1, point[1]];
-    if (inBounds(grid, toAdd)) {
-      neighbours.push({ next: toAdd, direction: "d" });
-    }
-  }
-  return neighbours;
-}
-
 const part1 = (rawInput) => {
   const input = parseInput(rawInput)
     .split("\n")
     .map((x) => x.split("").map(Number));
-  const startX = 0;
-  const startY = 0;
-  const endX = input[0].length - 1;
-  const endY = input.length - 1;
-  distanceToEnd(input, [startX, startY], [endX, endY], [0, 0], 0);
-  return best;
+  const costs = triplePushingDijkstra(input);
+  return costs;
 };
 
 const part2 = (rawInput) => {
-  const input = parseInput(rawInput);
+  const input = parseInput(rawInput)
+    .split("\n")
+    .map((x) => x.split("").map(Number));
 
-  return;
+  const costs = megaCruciblePushingDijkstra(input);
+  return costs;
 };
+function inBounds(grid, y, x) {
+  return y >= 0 && y < grid.length && x >= 0 && x < grid[0].length;
+}
 
+const directions = [
+  [-1, 0],
+  [0, -1],
+  [0, 1],
+  [1, 0],
+];
+
+function sortPriorityQueue(a, b) {
+  return a[5] - b[5];
+}
+function atEnd(grid, y, x) {
+  return y === grid.length - 1 && x === grid[0].length - 1;
+}
+
+function triplePushingDijkstra(grid) {
+  const visited = new Set();
+  const pq = new Heap(sortPriorityQueue);
+  pq.push([0, 0, 0, 0, 0, 0]);
+  while (pq.length > 0) {
+    const [y, x, dy, dx, numSteps, heatLoss] = pq.pop();
+    if (atEnd(grid, y, x)) {
+      return heatLoss;
+    }
+    if (visited.has(JSON.stringify([y, x, dy, dx, numSteps]))) continue;
+    visited.add(JSON.stringify([y, x, dy, dx, numSteps]));
+    if (numSteps < 3 && !(dx === 0 && dy === 0)) {
+      if (inBounds(grid, y + dy, x + dx)) {
+        const cost = heatLoss + grid[y + dy][x + dx];
+        pq.push([y + dy, x + dx, dy, dx, numSteps + 1, cost]);
+      }
+    }
+    for (const [a, b] of directions) {
+      if (a === -dy && b === -dx) continue;
+      if (a === dy && b === dx) continue;
+      const [y1, x1] = [y + a, x + b];
+      if (!inBounds(grid, y1, x1)) continue;
+      const cost = heatLoss + grid[y1][x1];
+      pq.push([y1, x1, a, b, 1, cost]);
+    }
+  }
+}
+function sumNextFour(grid, y, x, dy, dx) {
+  let sum = 0;
+  for (let i = 1; i <= 4; i++) {
+    const [ny, nx] = [y + i * dy, x + i * dx];
+    if (inBounds(grid, ny, nx)) {
+      sum += grid[ny][nx];
+    }
+  }
+  return sum;
+}
+function megaCruciblePushingDijkstra(grid) {
+  const visited = new Set();
+  const pq = new Heap(sortPriorityQueue);
+  pq.push([0, 0, 0, 0, 0, 0]);
+  while (pq.length > 0) {
+    const [y, x, dy, dx, numSteps, heatLoss] = pq.pop();
+    if (atEnd(grid, y, x)) {
+      return heatLoss;
+    }
+    if (visited.has(JSON.stringify([y, x, dy, dx, numSteps]))) continue;
+    visited.add(JSON.stringify([y, x, dy, dx, numSteps]));
+
+    if (numSteps < 10 && numSteps >= 4) {
+      if (inBounds(grid, y + dy, x + dx)) {
+        const cost = heatLoss + grid[y + dy][x + dx];
+        pq.push([y + dy, x + dx, dy, dx, numSteps + 1, cost]);
+      }
+    }
+    for (const [a, b] of directions) {
+      if (a === -dy && b === -dx) continue;
+      if (a === dy && b === dx) continue;
+      const ny = y + a * 4;
+      const nx = x + b * 4;
+      if (!inBounds(grid, ny, nx)) continue;
+      const cost = heatLoss + sumNextFour(grid, y, x, a, b);
+      pq.push([ny, nx, a, b, 4, cost]);
+    }
+  }
+}
 run({
   part1: {
     tests: [
@@ -132,15 +114,71 @@ run({
         `,
         expected: 8,
       },
+      {
+        input: `
+          1531
+          1111
+          1234
+        `,
+        expected: 8,
+      },
+      {
+        input: `
+        2413432311323
+        3215453535623
+        3255245654254
+        3446585845452
+        4546657867536
+        1438598798454
+        4457876987766
+        3637877979653
+        4654967986887
+        4564679986453
+        1224686865563
+        2546548887735
+        4322674655533
+        `,
+        expected: 102,
+      },
     ],
     solution: part1,
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `
+        2413432311323
+        3215453535623
+        3255245654254
+        3446585845452
+        4546657867536
+        1438598798454
+        4457876987766
+        3637877979653
+        4654967986887
+        4564679986453
+        1224686865563
+        2546548887735
+        4322674655533
+        `,
+        expected: 94,
+      },
+      {
+        input: `
+        111111111111
+        999999999991
+        999999999991
+        999999999991
+        999999999991
+        `,
+        expected: 71,
+      },
+      {
+        input: `
+          12341
+        `,
+        expected: 10,
+      },
     ],
     solution: part2,
   },
